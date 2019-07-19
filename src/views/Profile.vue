1876a397d98b85e59f31f4f1981cfaf610188d7a
Leo Goemaere <lgoemaere@itelios.com>
2019-07-19 17:32:49 +0200

    Add heading on my profile view

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-	  Profile
-  </div>
+	<div class="profile">
+		<div class="heading space-x">
+			<h1>My Profile</h1>
+			<router-link to="/foods/add" class="add-button">+</router-link>
+			<transition name="sliding-on-side">
+				<router-view></router-view>
+			</transition>
+		</div>
+	</div>
 </template>
