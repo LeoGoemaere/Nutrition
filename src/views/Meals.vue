1bbb6c94187cbb276723741497402312c0196381
Leo Goemaere <lgoemaere@itelios.com>
2019-07-26 11:48:29 +0200

    Dynamise meals view

--- src/views/Meals.vue
+++ src/views/Meals.vue
@@ -3,15 +3,19 @@
 		<div class="heading space-x">
 			<h1>My Meals</h1>
 			<router-link to="/meals/add" class="add-button">+</router-link>
-
 			<transition name="sliding-on-top">
 				<router-view></router-view>
 			</transition>
 		</div>
 		<div class="meal__list">
-			<div class="meal__row space-x meal__row--active">
+			<div
+				v-for="meal in getMeals"
+				:key="meal.key"
+				@click="toggleAccordion($event)"
+				class="meal__row space-x"
+			>
 				<div class="meal__title">
-					<span>Breakfast</span>
+					<span>{{meal.name}}</span>
 					<div class="meal__edit-container">
 						<button class="ui-bar-button u-mr">Edit</button>
 						<i class="fas fa-chevron-down icon-chevron"></i>
@@ -21,56 +25,33 @@
 					<div class="meal__recap">
 						<p class="nutriments__label">
 							<i class="fas fa-fire-alt meal__recap-icon"></i>
-							<span class="meal__datas">500kj / 250kcal</span>
+							<span class="meal__datas">
+								{{roundValue(convertKilojouleToKilocalorie(sum(meal.foods.map((food) => food.nutriments.energy_100g))))}} kcal
+							</span>
 						</p>
 						<p class="nutriments__label">
 							<i class="fas fa-drumstick-bite meal__recap-icon"></i>
-							<span class="meal__datas">10g</span>
+							<span class="meal__datas">{{roundValue(sum(meal.foods.map((food) => food.nutriments.proteins_100g)))}}</span>
 						</p>
 						<p class="nutriments__label">
 							<i class="fas fa-bread-slice meal__recap-icon"></i>
-							<span class="meal__datas">10g</span>
+							<span class="meal__datas">{{roundValue(sum(meal.foods.map((food) => food.nutriments.carbohydrates_100g)))}}</span>
 						</p>
 						<p class="nutriments__label">
 							<i class="fas fa-fish meal__recap-icon"></i>
-							<span class="meal__datas">3g</span>
+							<span class="meal__datas">{{roundValue(sum(meal.foods.map((food) => food.nutriments.fat_100g)))}}</span>
 						</p>
 					</div>
-					<div class="foods__row">
-						<img class="foods__image" src="https://static.openfoodfacts.org/images/products/761/303/462/6844/front_fr.71.400.jpg" alt="">
-						<div class="foods__name">Chocapic</div>
+					<div 
+						v-for="food in meal.foods"
+						:key="food.key"
+						class="foods__row"
+					>
+						<img class="foods__image" :src="food.image_url" alt="">
+						<div class="foods__name">{{food.product_name}}</div>
 						<div class="u-mr u-ml">
-							<!-- <router-link :to="{ name: 'details' }" class="search__results-btn-detail"> -->
-								<i class="far fa-question-circle"></i>
-							<!-- </router-link> -->
+							<i class="far fa-question-circle"></i>
 						</div>
-					</div>
-					<div class="foods__row">
-						<img class="foods__image" src="https://static.openfoodfacts.org/images/products/761/303/462/6844/front_fr.71.400.jpg" alt="">
-						<div class="foods__name">Chocapic</div>
-						<div class="u-mr u-ml">
-							<!-- <router-link :to="{ name: 'details' }" class="search__results-btn-detail"> -->
-								<i class="far fa-question-circle"></i>
-							<!-- </router-link> -->
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="meal__row space-x">
-				<div class="meal__title">
-					<span>Lunch</span>
-					<div class="meal__edit-container">
-						<button class="ui-bar-button u-mr">Edit</button>
-						<i class="fas fa-chevron-down icon-chevron"></i>
-					</div>
-				</div>
-			</div>
-			<div class="meal__row space-x">
-				<div class="meal__title">
-					<span>Lunch</span>
-					<div class="meal__edit-container">
-						<button class="ui-bar-button u-mr">Edit</button>
-						<i class="fas fa-chevron-down icon-chevron"></i>
 					</div>
 				</div>
 			</div>
@@ -84,6 +65,21 @@ import { mapGetters } from 'vuex';
 export default {
 	name: 'meals',
 	components: {
+	},
+	methods: {
+		convertKilojouleToKilocalorie: function(kilojoule) {
+			return kilojoule / 4.184;
+		},
+		roundValue: function(value) {
+			return Math.ceil(value * 10) / 10;
+		},
+		toggleAccordion: function(e) {
+			e.currentTarget.classList.toggle('meal__row--active');
+		},
+		sum(array) {
+			// Using parseFloat because it's possible to get a string value.
+			return array.reduce((accumulator, currentValue) => parseFloat(accumulator, 10) + parseFloat(currentValue, 10), 0);
+		}
 	},
 	computed: {
 		...mapGetters([
